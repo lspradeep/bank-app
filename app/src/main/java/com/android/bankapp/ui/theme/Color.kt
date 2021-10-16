9985c8b0f905049ceca79235e56c23ec0c26da0b
pradeep <lspradeep21@gmail.com>
2021-10-16 11:31:09 +0530

    Second commit

--- app/src/main/java/com/android/bankapp/ui/theme/Color.kt
+++ app/src/main/java/com/android/bankapp/ui/theme/Color.kt
@@ -2,9 +2,9 @@ package com.android.bankapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val primaryColorDark = Color(0xFF8692FC)
-val primaryColor = Color(0xFF1157AD)
-val primaryVariant = Color(0xFF0048B3)
+val primaryColorDark = Color(0xFF637BFA)
+val primaryColor = Color(0xFF4D63E8)
+val primaryVariant = Color(0xFF4D63E8)
 val secondaryColor = Color(0xFF03DAC5)
 
 val lightGray = Color(0xFFD8DDDC)
